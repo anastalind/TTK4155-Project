f00a2aaba598fb68b4240a6fb091385c5ab77fb1
Anastasia <anastal@stud.ntnu.no>
2019-10-18 09:45:25 +0200

    Tried changing name of makefile in node 2

--- Node2/main.c
+++ Node2/main.c
@@ -18,16 +18,14 @@ void main() {
 
     USART_init(9600);
     printf("Whatever\n\r");
+
     CAN_init();
+
     while(1){
         message position = CAN_data_receive();
-        for (int i=0; i < position.length; i++){
+        for (int i = 0; i < position.length; i++){
             printf("Position: %u\n\r",position.data[i]);
             printf("Id: %u\n\r",position.id);
         }
-    }
-    
-    
+    }       
 }
-
-
